bd62798ab29d1ca73da084063d03b0d2dd47d75e
Victor Nguyen <vic.nguyen@gmail.com>
2012-08-30 02:02:25 +1000

    tweaking twitter names

--- src/Website/models/players.clj
+++ src/Website/models/players.clj
@@ -1,8 +1,8 @@
 (ns Website.models.players)
 
 (def players[
-			{:image "img/jay.jpg" :player-name "jay" :twitter-name "Jaypet"}
+			{:image "img/jay.jpg" :player-name "jay" :twitter-name "jaypet"}
 			{:image "/photos/aaron/001.jpg" :player-name "aaron" :twitter-name "Gravypower"}
-			{:image "img/rob.jpg" :player-name "rob" :twitter-name "Robearlam"}
-			{:image "img/vic.jpg" :player-name "vic" :twitter-name "Victornguyen"}
-			{:image "img/pete.jpg" :player-name "pete" :twitter-name "Petecostello"}])
+			{:image "img/rob.jpg" :player-name "rob" :twitter-name "RobEarlam"}
+			{:image "img/vic.jpg" :player-name "vic" :twitter-name "victornguyen"}
+			{:image "img/pete.jpg" :player-name "pete" :twitter-name "petecostello"}])
