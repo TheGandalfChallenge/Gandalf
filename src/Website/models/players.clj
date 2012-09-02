4ceee91d7f44d3e6a96f9dca20aed3b76297fc0b
Victor Nguyen <vic.nguyen@gmail.com>
2012-09-02 23:36:17 +1000

    added hardcore's photos

--- src/Website/models/players.clj
+++ src/Website/models/players.clj
@@ -3,6 +3,6 @@
 (def players[
 			{:image "/photos/jay/001.jpg" :player-name "jay" :twitter-name "jaypet"}
 			{:image "/photos/aaron/001.jpg" :player-name "aaron" :twitter-name "Gravypower"}
-			{:image "img/rob.jpg" :player-name "rob" :twitter-name "RobEarlam"}
+			{:image "/photos/rob/001.jpg" :player-name "rob" :twitter-name "RobEarlam"}
 			{:image "img/vic.jpg" :player-name "vic" :twitter-name "victornguyen"}
 			{:image "/photos/pete/001.jpg" :player-name "pete" :twitter-name "petecostello"}])
