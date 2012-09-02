601648fb39f94fc69c4794a000f2be8839af0c45
Victor Nguyen <vic.nguyen@gmail.com>
2012-09-02 22:48:03 +1000

    added jay's photos

--- src/Website/models/players.clj
+++ src/Website/models/players.clj
@@ -1,7 +1,7 @@
 (ns Website.models.players)
 
 (def players[
-			{:image "img/jay.jpg" :player-name "jay" :twitter-name "jaypet"}
+			{:image "/photos/jay/001.jpg" :player-name "jay" :twitter-name "jaypet"}
 			{:image "/photos/aaron/001.jpg" :player-name "aaron" :twitter-name "Gravypower"}
 			{:image "img/rob.jpg" :player-name "rob" :twitter-name "RobEarlam"}
 			{:image "img/vic.jpg" :player-name "vic" :twitter-name "victornguyen"}
